8313dda9ab23e38f29d9d7775801706a1f052bcb
sradms0 <sradms0@openmailbox.org>
2019-01-12 12:09:41 -0500

    add courses api calls

--- client/src/apiRequests.js
+++ client/src/apiRequests.js
@@ -6,3 +6,11 @@ export const user = {
   register: data => axios.post('/api/users', data),
   profile: () => axios.get('/api/users')
 };
+
+export const courses = {
+  all: () => axios.get('/api/courses'),
+  single: courseId => axios.get(`/api/courses/${courseId}`),
+  update: (courseId, data) => axios.put(`/api/courses/${courseId}`),
+  delete: courseId => axios.delete(`/api/courses/${courseId}`),
+  register: data => axios.post('/api/courses', data)
+}
