73f29f9c780faa57e3f8a4a4821336f026c70d9b
sradms0 <sradms0@openmailbox.org>
2019-01-14 16:19:16 -0500

    add /api/terms route and allSectionTerms to get method

--- server/routes/term.js
+++ server/routes/term.js
@@ -4,11 +4,14 @@ const middleware = require('./middleware');
 const controller = require('../controllers/term');
 
 module.exports = app => {
-  app.route('/api/section/:sectionId/term')
+  app.route('/api/terms')
     .get(middleware.loginRequired, controller.allTerms)
+
+  app.route('/api/sections/:sectionId/terms')
+    .get(middleware.loginRequired, controller.allSectionTerms)
     .post(middleware.loginRequired, controller.registerTerm);
 
-  app.route('/api/section/:sectionId/Term/:TermId')
+  app.route('/api/sections/:sectionId/terms/:termId')
     .get(middleware.loginRequired, controller.singleTerm)
     .put(middleware.loginRequired, controller.updateTerm)
     .delete(middleware.loginRequired, controller.deleteTerm);
