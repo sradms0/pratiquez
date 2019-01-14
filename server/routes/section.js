c812d9be42cdeb5a7456637455d1a8add16292f6
sradms0 <sradms0@openmailbox.org>
2019-01-13 19:43:30 -0500

    add /api/sections

--- server/routes/section.js
+++ server/routes/section.js
@@ -5,8 +5,11 @@ const controller = require('../controllers/section');
 
 module.exports = app => {
   app.route('/api/courses/:courseId/sections')
-    .get(middleware.loginRequired, controller.allSections)
+    .get(middleware.loginRequired, controller.allCourseSections)
     .post(middleware.loginRequired, controller.registerSection);
+
+  app.route('/api/sections')
+    .get(middleware.loginRequired, controller.allSections);
 
   app.route('/api/sections/:sectionId')
     .get(middleware.loginRequired, controller.singleSection)
