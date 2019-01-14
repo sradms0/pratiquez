19dd564ec72b15892d28b32a9c4db1994136e4c3
sradms0 <sradms0@openmailbox.org>
2019-01-14 13:55:35 -0500

    create separate component for modal items

---
+++ client/src/components/ModalCourseItem.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { List, Modal } from 'semantic-ui-react';
+
+export default function ModalCourseItem(props) {
+  return (
+    <Modal 
+      size='tiny'
+      open={props.modalOpen}
+      onClose={props.modalOnClose}
+    >
+      <Modal.Header align='center'>{props.modalTitle}</Modal.Header>
+      <Modal.Content align='center'>
+        <List divided>
+          <List.Item>
+              <List.Content>
+                <List.Header>
+                  title: {props.modalTitle}
+                </List.Header>
+                {props.titleAddForm()}
+              </List.Content>
+          </List.Item>
+
+            <List.Item>
+              <List.Header>
+                sections: {props.sectionsCount}
+              </List.Header>
+              {props.sectionsAddView()}
+            </List.Item>
+        </List>
+      </Modal.Content>
+    </Modal>
+  );
+}
+
