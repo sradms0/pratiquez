ab868d519da3e9997b24c03b949553104f3a9bbd
sradms0 <sradms0@openmailbox.org>
2019-01-17 17:44:32 -0500

    add /api/notes get route and pass allNotes and pass allSectionNotes to /api/sections/:sectionId/notes

--- server/routes/note.js
+++ server/routes/note.js
@@ -5,8 +5,11 @@ const controller = require('../controllers/note');
 
 module.exports = app => {
   app.route('/api/sections/:sectionId/notes')
-    .get(middleware.loginRequired, controller.allNotes)
+    .get(middleware.loginRequired, controller.allSectionNotes)
     .post(middleware.loginRequired, controller.registerNote);
+
+  app.route('/api/notes')
+    .get(middleware.loginRequired, controller.allNotes);
 
   app.route('/api/notes/:noteId')
     .get(middleware.loginRequired, controller.singleNote)
