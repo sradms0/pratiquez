48bcf5e16461a82ff5cfeae7416630f0b693ef7e
sradms0 <sradms0@openmailbox.org>
2019-01-17 17:23:26 -0500

    add remapping of yt object if search prop is true

--- client/src/components/VideoList.js
+++ client/src/components/VideoList.js
@@ -3,10 +3,24 @@ import { List } from 'semantic-ui-react';
 import VideoItem from './VideoItem';
 
 export default function VideoList(props) {
-  const { videos } = props;
-  if (videos)  {
+  const { data, searched } = props;
+
+  if (data)  {
+    let videos = data;
+    // not super efficent 
+    // (would like to make video schema 
+    // in mongo represent youtube video object exactly to avoid remapping)
+    if (searched) {
+      videos = data.map(data => ({
+          videoId:      data.id.videoId,
+          thumbnailURL: data.snippet.thumbnails.default.url,
+          title:        data.snippet.title,
+          description:  data.snippet.description
+        })
+      );
+    }
     const videoItems = videos.map(video => (
-      <VideoItem key={video.etag} video={video}/>
+      <VideoItem searched={props.searched} key={video.videoId} video={video}/>
     ));
     return (
       <List celled>
