2acda6904e58530ba8bd6ff68d6eba32ea0d27d0
sradms0 <sradms0@openmailbox.org>
2019-01-18 11:14:07 -0500

    add /api/videos/:videoId route and attach delete request

--- server/routes/video.js
+++ server/routes/video.js
@@ -4,6 +4,9 @@ const middleware = require('./middleware');
 const controller = require('../controllers/video');
 
 module.exports = app => {
+  app.route('/api/videos/:videoId/')
+    .delete(middleware.loginRequired, controller.deleteVideo);
+
   app.route('/api/notes/:noteId/videos')
     .post(middleware.loginRequired, controller.registerVideo);
 };
