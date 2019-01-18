433d80693bdbb6f84437f15c178ec260c09fe679
sradms0 <sradms0@openmailbox.org>
2019-01-18 11:07:26 -0500

    change to stateful component to enable nested modal video searching

--- client/src/components/ModalNoteItem.js
+++ client/src/components/ModalNoteItem.js
@@ -1,51 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Container, List, Modal } from 'semantic-ui-react';
 
 // Components
-import VideoSearch from './VideoSearch';
+import ModalVideoSearch from './ModalVideoSearch';
 
-export default function ModalNoteItem(props) {
-  return (
-    <Modal 
-      size='tiny'
-      open={props.modalOpen}
-      onClose={props.modalOnClose}
-    >
-      <Modal.Header align='center'>{props.modalTitle}</Modal.Header>
-      <Modal.Content align='center'>
-        <List divided>
-          <List.Item>
+export default class ModalNoteItem extends Component {
+  state = { 
+    modalVideoSearch: { open: false } 
+  }
+
+  modalVideoSearchOnClose = () => {
+    if (this.state.modalVideoSearch.open) this.modalVideoSearchToggleOpen()
+  }
+
+ modalVideoSearchToggleOpen = () => {
+    this.setState(prevState => ({ modalVideoSearch: {open: !prevState.modalVideoSearch.open} }));
+  }
+
+  render() {
+    return (
+      <Modal 
+        size='tiny'
+        open={this.props.modalOpen}
+        onClose={this.props.modalOnClose}
+      >
+        <Modal.Header align='center'>{this.props.modalTitle}</Modal.Header>
+        <Modal.Content align='center'>
+          <List divided>
+            <List.Item>
+                <List.Content>
+                  <List.Header>
+                    title: {this.props.modalTitle}
+                  </List.Header>
+                  {this.props.noteAddForm()}
+                </List.Content>
+            </List.Item>
+
+            <List.Item>
               <List.Content>
                 <List.Header>
-                  title: {props.modalTitle}
+                  videos: {this.props.videoCount}
+                  <Container align='center'>
+                    <Button onClick={this.modalVideoSearchToggleOpen}color='green' icon='plus' size='mini' />
+                  </Container>
                 </List.Header>
-                {props.noteAddForm()}
+                {this.props.videoCount ? this.props.videosAddView() : null}
+
+                <ModalVideoSearch
+                  modalVideoSearchOnClose={this.modalVideoSearchOnClose}
+                  modalVideoSearchOpen={this.state.modalVideoSearch.open}
+                  modalVideoSearchToggleOpen={this.modalVideoSearchToggleOpen}
+                /> 
+      
               </List.Content>
-          </List.Item>
+            </List.Item>
 
-          <List.Item>
-            <List.Content>
+            <List.Item>
               <List.Header>
-                videos: {props.videoCount}
-                <Container align='center'>
-                  <Button color='green' icon='plus' size='mini' />
-                </Container>
+                {/*TODO: something...*/}
               </List.Header>
-              {props.videoCount ? props.videosAddView() : null}
-            </List.Content>
-          </List.Item>
-
-          <List.Item>
-            <List.Header>
-              {/*TODO: something...*/}
-            </List.Header>
-            <List.Content>
-              {props.sectionCourseAddView()}
-            </List.Content>
-          </List.Item>
-        </List>
-      </Modal.Content>
-    </Modal>
-  );
+              <List.Content>
+                {this.props.sectionCourseAddView()}
+              </List.Content>
+            </List.Item>
+          </List>
+        </Modal.Content>
+      </Modal>
+    );
+  }
 }
-
