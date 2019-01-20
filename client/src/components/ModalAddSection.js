a83565cbcf9a430d06a3ba08fe13c8bcc5ed1933
sradms0 <sradms0@openmailbox.org>
2019-01-20 13:31:40 -0500

    add modal section form functionality

---
+++ client/src/components/ModalAddSection.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import { Button, Form, Grid, Message, Modal, Segment } from 'semantic-ui-react'
+
+export default class ModalAddSection extends Component {
+  state = {
+    title: '',
+    courseId: '',
+    courseOptions: []
+  }
+
+  componentDidMount() {
+    this.setCourseData();
+  }
+
+  componentWillUnmount() {
+    this.props.errState('clear');
+  }
+
+  setCourseData = () => {
+    this.props.api.courses.all()
+      .then(res => {
+        const options = res.data.map(course => (
+          {
+            key: course._id, 
+            text: course.title,
+            value: course._id
+          }
+        ));
+
+        this.setState({ courseOptions: [...options] })
+      })
+      .catch(err => this.props.errState('set', err));
+  }
+
+  titleOnChangeHandler = e => this.setState({ title: e.target.value });
+
+  courseIdOnChangeHandler = (e, data) => this.setState({ courseId: data.value });
+
+  submitHandler = e => {
+    const data = { title: this.state.title };
+    this.props.api.sections.register(this.state.courseId, data)
+      .then(() => {
+        this.props.updateList('sections');
+        this.props.updateList('courses');
+        this.props.modalAddSectionToggleOpen();
+      })
+      .catch(err => this.props.errState('set', err));
+  };
+
+  render() {
+    return (
+      <Modal 
+        size='tiny'
+        open={this.props.modalAddSectionOpen}
+        onClose={this.props.modalAddSectionOnClose}
+      >
+        <Modal.Header align='center'>Create a Section</Modal.Header>
+        <Modal.Content align='center'>
+          <Form size='large' onSubmit={this.submitHandler} error={this.props.err ? true : false}>
+            <Message
+              error
+              content={this.props.err}
+            />
+            <Form.Input
+              fluid
+              label='title'
+              name='title'
+              onChange={this.titleOnChangeHandler}
+              required
+            />
+            <Form.Select
+              fluid
+              label='courses'
+              name='courses'
+              options={this.state.courseOptions}
+              placeholder='select a course...'
+              onChange={this.courseIdOnChangeHandler}
+              required
+            />
+            <Button type="submit" size='small' color='teal'>
+              Create Section
+            </Button>
+          </Form>
+        </Modal.Content>
+      </Modal>
+    );
+  }
+}
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,20 +4,30 @@ import { Link } from 'react-router-dom';
 import axios from  'axios';
 
 // Components
-import ModalAddCourse from './ModalAddCourse';
+import ModalAddCourse   from './ModalAddCourse';
+import ModalAddSection  from './ModalAddSection';
 
 export default class Nav extends Component {
   state = { 
    activeItem: null,
-   modalAddCourse: { open: false }
+   modalAddCourse:  { open: false },
+   modalAddSection: { open: false }
   }
 
   modalAddCourseToggleOpen = () => {
     this.setState(prevState => ({ modalAddCourse: {open: !prevState.modalAddCourse.open} }))
   }
 
+  modalAddSectionToggleOpen = () => {
+    this.setState(prevState => ({ modalAddSection: {open: !prevState.modalAddSection.open} }))
+  }
+
   modalAddCourseOnClose = () => {
     if (this.state.modalAddCourse.open) this.modalAddCourseToggleOpen();
+  }
+
+  modalAddSectionOnClose = () => {
+    if (this.state.modalAddSection.open) this.modalAddSectionToggleOpen();
   }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
@@ -39,6 +49,7 @@ export default class Nav extends Component {
         <Dropdown icon='plus' item button>
           <Dropdown.Menu>
             <Dropdown.Item icon='book' text='Course' onClick={this.modalAddCourseToggleOpen}/>
+            <Dropdown.Item icon='bookmark' text='Section' onClick={this.modalAddSectionToggleOpen}/>
           </Dropdown.Menu>
         </Dropdown>
 
@@ -62,6 +73,16 @@ export default class Nav extends Component {
           api={this.props.api}
           updateList={this.props.updateList}
         />
+
+        <ModalAddSection 
+          modalAddSectionOnClose={this.modalAddSectionOnClose}
+          modalAddSectionOpen={this.state.modalAddSection.open}
+          modalAddSectionToggleOpen={this.modalAddSectionToggleOpen}
+
+          api={this.props.api}
+          updateList={this.props.updateList}
+        />
+
       </Menu>
     )
   }
