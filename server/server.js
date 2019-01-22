161910a28fca9b1edef19174be0325dff7f3c1b3
sradms0 <sradms0@openmailbox.org>
2019-01-21 20:38:49 -0500

    clean up/try to fix routing issues..

--- server/server.js
+++ server/server.js
@@ -11,14 +11,6 @@ const path        = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// serve static files from react app
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('../client/build'));
-}
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-
 // db,secret config
 const { db, secret } = require('./config/keys');
 
@@ -74,6 +66,15 @@ app.use((err, req, res, next) => {
   });
   next();
 });
+
+// serve static files from react app
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('../client/build'));
+
+  app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+  });
+}
 
 // start server
 app.listen(port, () => {
